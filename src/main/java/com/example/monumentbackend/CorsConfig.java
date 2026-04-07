198f3d14701d874896fc7163386253ac41e48652
bsirisampath <2400031026@kluniversity.in>
2026-04-07 23:17:59 +0530

    added cors config and fixed backend

---
+++ src/main/java/com/example/monumentbackend/CorsConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("https://bharath-heritage-website-ec3y.vercel.app/")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
